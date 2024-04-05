9a3697b2a6c3332e7c12e3ecf0a3c1ebcdba7101
AviKurt <avi.kurt@petpace.com>
2024-04-05 13:38:21 +0300

    Initialized spring boot project

---
+++ src/main/java/com/criteo/sponsoredads/SponsoredAdsApplication.java
@@ -0,0 +1,13 @@
+package com.criteo.sponsoredads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SponsoredAdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SponsoredAdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/criteo/sponsoredads/SponsoredAdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criteo.sponsoredads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SponsoredAdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
