b125e0e874b904069872b07adf0baefd7a764d93
AviKurt <avi.kurt@petpace.com>
2024-04-06 19:29:52 +0300

    Added HandlerMethodValidationException handling

--- src/main/java/com/criteo/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/criteo/exceptions/GlobalExceptionHandler.java
@@ -6,24 +6,32 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 @Slf4j
 @ResponseBody
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(value = FailedToCreateException.class)
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ValidResponse handleMethodValidationException(HandlerMethodValidationException ex) {
+
+        log.debug("Bad Request: ", ex);
+        return ValidResponse.of("Bad request: Please check request parameters.", ResponseCode.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(FailedToCreateException.class)
     public ValidResponse handleFailedToCreateExceptions(FailedToCreateException ex) {
 
-        log.error("FailedToCreateException: ", ex);
+        log.error("FailedToCreate: ", ex);
         return ValidResponse.of("Creation failed." +
                 "Please try again later or call support.", ResponseCode.FAILED_TO_CREATE);
     }
 
-    @ExceptionHandler(value = Exception.class)
+    @ExceptionHandler(Exception.class)
     public ValidResponse handleExceptions(Exception ex) {
 
-        log.error("Exception: {}, Message: {}", ex, ex.getMessage());
+        log.error("Exception: ", ex);
         return ValidResponse.ofError("Ops.. something went wrong." +
                 "Please try again later or call support.");
     }
